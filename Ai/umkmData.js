ef35076abb9c762df456df4e594b21e726fa721b
Athrysd <221656567+Athrysd@users.noreply.github.com>
2025-11-20 09:04:45 +0700

    modified:   Ai/umkmData.js

--- Ai/umkmData.js
+++ Ai/umkmData.js
@@ -1,27 +1,44 @@
 export const umkmData = [
   {
-    name: "Teh Rimba",
-    category: "Minuman",
-    location: "Jakarta",
+    name: "Mie Ayam Keraton",
+    category: "Makanan",
+    location: "Lokasi : Malaka Sari, Kec. Duren Sawit, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta \nBuka : 07.00 - 23.00",
     description:
-      "Produk minuman teh alami tanpa pengawet, dengan rasa khas daun teh rimba.",
+      "Mie Ayam Keraton hadir dengan konsep \"harga kaki lima, rasa bintang lima\" dengan varian menu yang disajikan secara komplet seperti mie ayam, bakso, ceker, dan pangsit, cocok dinikmati kapan saja.Varian Harga dan Menu \n Mie Ayam: Rp8.000-Rp13.000 \n Mie Ayam Bakso: Rp16.000-Rp18.750 \n Mie Ayam Komplit: Rp18.000-Rp28.750 \n Ceker: Rp7.500-Rp21.250",
   },
   {
-    name: "Hafidz Store",
-    category: "Fashion",
-    location: "Bandung",
-    description: "UMKM fashion dengan produk tas dan aksesoris kulit handmade.",
+    name: "Nasi Kuning dan Nasi Uduk JJM",
+    category: "Makanan",
+    location: "Lokasi : Jl. Dermaga Raya No.99A, RT.5, Klender, Kec. Duren Sawit, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13470\nBuka : 18.00 - 22.00",
+    description: "Nikmati seblak pedas dengan cita rasa khas yang bikin melek dan mewek! Isian lengkap mulai dari kerupuk kenyal, mie, bakso, hingga ceker, dimasak dengan bumbu pedas gurih yang menggugah selera.\n Varian Harga dan Menu\n berbagai paket seblak mulai dari Rp6.000,\n dan Banyak pilihan toping mulai dari Rp2.000"
   },
   {
-    name: "Teh Manis Jumbo",
-    category: "Minuman",
+    name: "Seblak Mewek",
+    category: "Makanan",
     location: "Yogyakarta",
     description: "Brand teh jumbo dengan rasa manis pas dan kemasan besar.",
   },
   {
-    name: "Dapur Lestari",
-    category: "Kuliner",
-    location: "Surabaya",
-    description: "UMKM makanan tradisional dengan cita rasa khas Jawa Timur.",
+    name: "Dapoer Kita",
+    category: "Makanan",
+    location: "Lokasi : Blk. A Jl. Tenggiri No.3, RT.17/RW.6, Jati, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220\n Buka : 10.00-23.00",
+    description: "Hadir sebagai tempat makan santai dengan beragam pilihan menu lezat dan minuman kekinian. Cocok untuk nongkrong, istirahat, atau sekadar menikmati makanan enak dengan harga yang ramah di kantong.\n Varian Harga dan Menu\n Dapoer Kite menyediakan berbagai macam makanan dan minuman mulai dari makanan ringan sampai berat, Harga mulai dari Rp25.000-Rp50.000",
+  },
+  {
+    name: "Mi Aceh Lamlo, Klender",
+    category: "Makanan",
+    location: "Lokasi : Blk. A Jl. Tenggiri No.3, RT.17/RW.6, Jati, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220\n Buka : 10.00-23.00",
+    description: "Hadir dengan cita rasa rempah khas Aceh yang kuat dan menggugah selera! Mie tebal nan kenyal dimasak dengan bumbu pedas gurih yang khas, menghadirkan sensasi rasa otentik di setiap suapan.\n Varian Harga dan Menu\n Mie Aceh Lamlo menyediakan dua tipe mie, mie rebus dengan toping daging, udang dan cumi Rp35.000, dan mie goreng dengan toping daging Rp23.000",
+  },
+  {
+    name: "Kopi Kirani",
+    category: "Minuman",
+    location: "Lokasi : Jl. H. Ismail No.52 7, RT.6/RW.3, Jatinegara, Kec. Cakung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13930 \n Buka : 10.00-22.00",
+    description: "Tempat nongkrong nyaman dengan suasana hangat dan tenang. Menyajikan beragam pilihan kopi, minuman kekinian, serta aneka makanan lezat yang cocok untuk teman ngopi. Pilihan pas untuk bersantai, belajar, atau kumpul bersama teman.\n Varian Harga dan Menu Kopi Kirani menyediakan berbagai menu kopi mulai dari Rp15.000 dan makanan mulai dari Rp5.000", },
+  {
+    name: "Pocket Coffee",
+    category: "Minuman",
+    location: "Lokasi : Jl. Anggrek II No.101, RT.003/RW.009, Jatiwarna, Kec. Pd. Melati, Kota Bks, Jawa Barat 17415 Buka : 24 Jam pesan secara online",
+    description: "UMKM rumahan yang menyediakan Kopi praktis dengan cita rasa premium yang bisa dinikmati kapan saja! Dikemas ringkas dan siap saji, Pocket Coffee menghadirkan aroma kopi yang kuat dalam setiap tegukan. Dapatkan secara online.\n Varian Harga dan Menu Pocket Coffee menyediakan Kopi dengan berbagai rasa mulai dari Rp6.000-Rp12.000, dan juga Non-Kopi mulai dari Rp12.000",
   },
 ];
